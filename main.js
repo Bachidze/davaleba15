b5d056f1d975c33f734c2bd9fcfc78da4efa882d
bachidze <bachidze90@gmail.com>
2023-11-08 02:13:47 +0400

    shevcvale

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-function check()
-{
-  if(!document.getElementById("subscribe-email").checkValidity())
-  {
 
-    alert("input not valid! შეცვალე იმეილი!");
-    
-}else
-{
-    callMeIfValid();
+function checkEmailValidity() {
+  const emailInput = document.getElementById("subscribe-email");
+  const errorMessage = document.getElementById("error-message");
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+
+  if (!emailPattern.test(emailInput.value)) {
+    errorMessage.innerHTML = "Invalid email address";
+    errorMessage.style.color = 'red'
+  } else {
+    errorMessage.innerHTML = "Email Request Accepted";
+    errorMessage.style.color = 'green'
+  }
 }
-}
+
 
